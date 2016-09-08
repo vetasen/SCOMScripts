8055a959d3654b8f923db524e430ee286cbf8951
frank <frank@heima.net>
2016-09-08 10:40:36 +0200

    Added try-catch and errorhandling.

--- Powershell Monitor Scripts/Dev/Check-ClusteredServices.ps1
+++ Powershell Monitor Scripts/Dev/Check-ClusteredServices.ps1
@@ -1,35 +1,51 @@
 $API = new-object -comObject "MOM.ScriptAPI"
 $bag = $api.CreatePropertyBag()
 
+# Variables
+$Servicename = "Spooler"
+$Clustername = "SQL11"
 $clusternodes = @(
 "sql1";
 "brumlebass"
 )
-$Servicename = "Spooler"
+
+try {
+    test-connection -cn $Clustername -count 1 -erroraction STOP
+	}
+
+catch {
+    $TryError = $_.exception.message
+    }
+
+if ($TryError){
+    $bag.addvalue("ServiceError","Problemer med å nå kommunisere med $($Clustername). Feilmelding: $TryError")
+    $bag.addvalue("State","NoContact")
+    $bag
+    break
+}
 
 $ServiceStatus = get-service -cn $clusternodes -name $Servicename
 
 if ($ServiceStatus.status -notcontains "Stopped"){
-    $ServiceList=$null
+    $ServiceError=$null
         foreach ($service in $ServiceStatus){
-            $serviceList = "Tjenesten Spooler kjører på begge clusternodene."
+            $ServiceError = "Tjenesten $servicename kjører på begge clusternodene: $($clusternodes[0]) og $($clusternodes[1])"
         }
-        $bag.Addvalue("ServiceError","$Servicelist")
+        $bag.Addvalue("ServiceError","$ServiceError")
         $bag.addvalue("State","AllRunning")
         
 }
 
 elseif ($ServiceStatus.status -notcontains "Running"){
-    $ServiceList=$null
+    $ServiceError=$null
         foreach ($service in $ServiceStatus){
-            $serviceList = "Tjenesten Spooler er stoppet på begge clusternodene"
+            $ServiceError = "Tjenesten $ServiceName er stoppet på begge clusternodene: $($clusternodes[0]) og $($clusternodes[1])"
         }
-        $bag.Addvalue("ServiceError","$Servicelist")
+        $bag.Addvalue("ServiceError","$ServiceError")
         $bag.addvalue("State","AllStopped")
         
 }
 
 else {$bag.addvalue("State","Healthy")}  # Hvis det ikke finnes stoppede servicer, er alt i orden.
-
 
 $bag
